d15e33ef7b94bccd5d910730611458fae570f3e1
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-11-12 22:11:04 +0200

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,10 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a href='' className='link footer__link'>
+    <a
+      href='https://github.com/kiaanonthehub/kiaanonthehub-portfolio'
+      className='link footer__link'
+    >
       Kiaan Maharaj
     </a>
   </footer>
