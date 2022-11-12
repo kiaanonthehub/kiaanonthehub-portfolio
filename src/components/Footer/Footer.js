b10f6d56d8273beb1504160721fe1760139eb12c
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-11-12 20:15:32 +0200

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='' className='link footer__link'>
+      Kiaan Maharaj
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,92 @@
+const header = {
+  homepage: 'https://github.com/kiaanonthehub',
+  title: 'KM',
+}
+
+const about = {
+  name: 'Kiaan Maharaj',
+  role: 'Software Developer | Graduate ',
+  description:
+    'Why Should you hire me?' +
+    '\n Hi, I am Kiaan Maharaj a Graduate in Computer Science majoring in Application Development. I have 3 years of student development experince and 3 months of industry experince as a Junior Software Developer. In my free time outside campus , I keep myself busy developing and collaborating on side projects or learning new skills, to add my tech-stack. I have published two production applications on the Google Play Store. I think I would be a unique individual and an asset with loads of potential and growth in this graduate program. I believe , that by you affording me this opportunity to come on board, we can make great things happen together. I am never affraid to take on a challenge and I think youll should push me to do my best and watch me deliver as expected. ',
+  resume:
+    'https://github.com/kiaanonthehub/resume/blob/main/CV%20of%20Mr%20K%20Maharaj.pdf',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/kiaan-maharaj-b991341a7/',
+    github: 'https://github.com/kiaanonthehub',
+  },
+}
+
+const projects = [
+  {
+    name: 'Collect [IT]',
+    description:
+      'Collect[IT] is your personal collection management assistant. You can create a collection with a description and goal and then add items to your collection categories. This will streamline your collection process by keeping all of your collections and items in one simple app.',
+    stack: ['Android', 'Firebase', 'Firebase Auth.', 'Google Auth.', 'Java'],
+    sourceCode: 'https://github.com/kiaanonthehub/Collect-IT',
+    livePreview:
+      'https://play.google.com/store/apps/details?id=com.kiaan.collect_it&pli=1',
+  },
+  {
+    name: 'Locate [IT]',
+    description:
+      'Locate[IT] will help you to find locations and attractions near you. Our app will also allow you to search and bookmark your favourite locations and find the fastest route to a place.',
+    stack: ['Android', 'Google Maps API', 'Java', 'Firebase', 'Refrofit API'],
+    sourceCode: 'https://github.com/kiaanonthehub/LocateIT',
+    livePreview:
+      'https://play.google.com/store/apps/details?id=com.locateitteam.locateit',
+  },
+  {
+    name: 'Bulletin Board',
+    description:
+      'A bulletin board built using MEAN Stack (Mongo, ExpressJs, Angular, NodeJs). Implemented additional security features like Brute, Logging , Helment etc. This application demonstrates the features of advanced security practices. ',
+    stack: [
+      'Web Application',
+      'Mongo',
+      'ExpressJs',
+      'AngularJs',
+      'NodeJs',
+      'TypeScript',
+      'HTML',
+      'CSS',
+      'Bootstrap',
+    ],
+    sourceCode: 'https://github.com/kiaanonthehub/BulletinBoard.git',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'C#',
+  'Java',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'MVC',
+  'WPF',
+  'Git',
+  'NodeJs',
+  'AnguarJs',
+  'ExpressJs',
+  'TSQL',
+  'PL/SQL',
+  'Firebase',
+  'Mongo',
+  'Git',
+  'GitHub',
+  'Visual Studio',
+  'Visual Studio Code',
+  'Oracle',
+  'SQL Serve Management Studio',
+  'Agile',
+  'SDLC',
+  'Project Management',
+]
+
+const contact = {
+  email: 'kiaan.maharaj@outlook.com',
+}
+
+export { header, about, projects, skills, contact }
