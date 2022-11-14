55dff5dc5524ad463e46b80987a32dc89519e84a
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-11-14 11:27:50 +0200

    updated repo access to redirect to github acc

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/kiaanonthehub/kiaanonthehub-portfolio'
+      href='https://github.com/kiaanonthehub'
       className='link footer__link'
     >
       Kiaan Maharaj
